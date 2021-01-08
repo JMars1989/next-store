0d986cf793d75d2cfc8141667e2575aa24d6c2c9
JMars1989 <jamescmarshall.iv@gmail.com>
2021-01-08 16:08:51 -0600

    add serverless function to test if correct key

---
+++ functions/test.js
@@ -0,0 +1,8 @@
+const { default: Product } = require("../pages/products/[product]")
+
+exports.handler = async (event, context) => {
+    return {
+        statusCode: 200,
+        body: `public key: ${process.env.STRIPE_PUBLIC_KEY}`
+    };
+};
