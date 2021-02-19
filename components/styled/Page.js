9052edf572643fe93de545592d1fcfe258824de2
JMars1989 <jamescmarshall.iv@gmail.com>
2021-02-19 16:43:19 -0600

    add responsive radio size selection + style

--- components/styled/Page.js
+++ components/styled/Page.js
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 
 const Page = styled.div`
-    background: white;
+    background: #3d3d3d;
     padding: 1rem 2rem;
     padding: 1rem 0rem;
+    color: #e0e0e0;
 `
 export default Page;
